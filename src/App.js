f78a8fff31d2694f12a8b94a71ab29bb59bdbfc7
minbyeol <ambit6998@bizwizsystem.co.kr>
2023-09-26 18:15:32 +0900

    9월26일-9챕터까지

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Counter";
+import InputSample from "./InputSample";
+
+function App() {
+  return (
+    <>
+      <InputSample />
+    </>
+  );
+}
+
+export default App;
